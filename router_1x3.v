dd465456964487a801f726ddec137566815e4db1
Naveen Kumar <rnaveenpatil44@gmail.com>
2025-07-20 14:53:29 +0530

    router_1x3.v

--- router_1x3.v
+++ router_1x3.v
@@ -11,9 +11,9 @@ module router_1x3(
 	wire [2:0]write_enb;
 	wire [7:0] dout;
 
-	router_fifo FIFO_0 (.clock(clock), .resetn(resetn), .write_enb(write_enb[0]), .soft_reset(soft_reset_0), .read_enb(read_enb_0), .data_in(data_out), .lfd_state(lfd), .empty(empty_0), .data_out(data_out_0), .full(full_0));
-	router_fifo FIFO_1 (.clock(clock), .resetn(resetn), .write_enb(write_enb[1]), .soft_reset(soft_reset_1), .read_enb(read_enb_1), .data_in(data_out), .lfd_state(lfd), .empty(empty_1), .data_out(data_out_1), .full(full_1));
-	router_fifo FIFO_2 (.clock(clock), .resetn(resetn), .write_enb(write_enb[2]), .soft_reset(soft_reset_2), .read_enb(read_enb_2), .data_in(data_out), .lfd_state(lfd), .empty(empty_2), .data_out(data_out_2), .full(full_2));
+	router_fifo FIFO_0 (.clock(clock), .resetn(resetn), .write_enb(write_enb[0]), .soft_reset(soft_reset_0), .read_enb(read_enb_0), .data_in(dout), .lfd_state(lfd), .empty(empty_0), .data_out(data_out_0), .full(full_0));
+	router_fifo FIFO_1 (.clock(clock), .resetn(resetn), .write_enb(write_enb[1]), .soft_reset(soft_reset_1), .read_enb(read_enb_1), .data_in(dout), .lfd_state(lfd), .empty(empty_1), .data_out(data_out_1), .full(full_1));
+	router_fifo FIFO_2 (.clock(clock), .resetn(resetn), .write_enb(write_enb[2]), .soft_reset(soft_reset_2), .read_enb(read_enb_2), .data_in(dout), .lfd_state(lfd), .empty(empty_2), .data_out(data_out_2), .full(full_2));
 
 	router_sync SYNCHRONISER (.clock(clock), .resetn(resetn), .write_enb_reg(write_enb_reg), .data_in(data_in[1:0]), .detect_add(detect_add), 
 					 .vld_out_0(valid_out_0), .vld_out_1(valid_out_1), .vld_out_2(valid_out_2), .read_enb_0(read_enb_0), .read_enb_1(read_enb_1), .read_enb_2(read_enb_2), 
